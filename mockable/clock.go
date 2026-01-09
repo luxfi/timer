257fab9d017848a7f58c4c1bc8573c06334e0d24
Zach Kelling <z@zeekay.io>
2026-01-09 14:50:46 -0800

    refactor: consolidate Meter type, remove duplicate interface
    - Remove meter.go (duplicate Meter interface)
    - Keep concrete Meter struct in clock.go
    - Add new timer files (adaptive, staged, stopped)

--- mockable/clock.go
+++ mockable/clock.go
@@ -1,37 +1,53 @@
 // Copyright (C) 2019-2025, Lux Industries, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-// Package mockable provides a mockable clock for testing.
 package mockable
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
-// MaxTime is the maximum representable time.
-var MaxTime = time.Unix(1<<63-62135596801, 0)
+// MaxTime was taken from https://stackoverflow.com/questions/25065055/what-is-the-maximum-time-time-in-go/32620397#32620397
+var MaxTime = time.Unix(1<<63-62135596801, 0) // 0 is used because we drop the nano-seconds
 
 // Clock acts as a thin wrapper around global time that allows for easy testing.
+// It is safe for concurrent use.
 type Clock struct {
+	mu    sync.RWMutex
 	faked bool
 	time  time.Time
 }
 
-// Set the time on the clock.
-func (c *Clock) Set(time time.Time) { c.faked = true; c.time = time }
+// Set the time on the clock
+func (c *Clock) Set(t time.Time) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.faked = true
+	c.time = t
+}
 
-// Sync this clock with global time.
-func (c *Clock) Sync() { c.faked = false }
+// Sync this clock with global time
+func (c *Clock) Sync() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.faked = false
+}
 
-// Time returns the time on this clock.
+// Time returns the time on this clock
 func (c *Clock) Time() time.Time {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	if c.faked {
 		return c.time
 	}
 	return time.Now()
 }
 
-// UnixTime returns the unix time on this clock truncated to seconds.
+// Time returns the unix time on this clock
 func (c *Clock) UnixTime() time.Time {
-	return c.Time().Truncate(time.Second)
+	resTime := c.Time()
+	return resTime.Truncate(time.Second)
 }
 
 // Unix returns the unix timestamp on this clock.
